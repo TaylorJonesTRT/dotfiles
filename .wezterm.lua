48c8b3dd5a0d6d459df3d4be5d7357f08507b644
Taylor Jones <9523996+TaylorJonesTRT@users.noreply.github.com>
2024-08-23 21:41:25 -0500

    now using wezterm as terminal emulator on macos

---
+++ .wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- Color Scheme Settings
+config.color_scheme = "Catppuccin Macchiato"
+-- config.window_background_image = "/Users/taylorjones/powder.jpg"
+-- config.window_background_opacity = 1.0
+
+config.background = {
+	{
+		source = {
+			Color = "rgba(36, 39, 58, 1)",
+		},
+		height = "100%",
+		width = "100%",
+	},
+	{
+		source = {
+			File = "/Users/taylorjones/powder.jpg",
+		},
+		opacity = 0.11,
+		width = "160%",
+		repeat_y = "NoRepeat",
+		vertical_align = "Middle",
+		horizontal_align = "Center",
+	},
+}
+
+return config
